45348a546d7decb814811157720ed74440aa4173
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-07-16 18:12:19 -0500

    Strings con Ecma script 6

---
+++ src/ECMAScript 6/03_strings.js
@@ -0,0 +1,30 @@
+/*
+|--------------------------------------------------------------------------
+| JS CLASICO
+|--------------------------------------------------------------------------
+*/
+
+var hello="hello"
+var word="word"
+var saludo=hello+" "+word
+console.log(saludo)
+
+var parrafo="Este es un gran día\n"+"para aprender Ecmascript"
+console.log(parrafo)
+
+/*
+|--------------------------------------------------------------------------
+| ECMAScript 
+|--------------------------------------------------------------------------
+*/
+
+//template literals
+
+let hello="hello"
+let word="word"
+let saludo=`${hello} ${word}`
+
+//multilinea
+let parrafo=`Este es un gran día
+para aprender Ecmascript`
+console.log(parrafo)
