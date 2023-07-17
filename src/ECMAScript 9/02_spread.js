fb81e6252b1d84338c29122a6156059f4ac97f93
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-07-16 23:32:00 -0500

    spread con ECMAScript 9

---
+++ src/ECMAScript 9/02_spread.js
@@ -0,0 +1,10 @@
+/*
+|--------------------------------------------------------------------------
+| ECMAScript
+|--------------------------------------------------------------------------
+*/
+
+const user = { username: "gndx", age: 34, coutry: "CO" };
+const { username, ...values } = user;
+console.log(username);
+console.log(values);
