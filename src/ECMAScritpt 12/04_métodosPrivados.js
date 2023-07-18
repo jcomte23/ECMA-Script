68474df3209686b2cd14100e08ad4f6f3f724301
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-07-18 11:04:05 -0500

    promise-any y métodos privados

---
+++ src/ECMAScritpt 12/04_métodosPrivados.js
@@ -0,0 +1,34 @@
+/*
+|--------------------------------------------------------------------------
+| ECMAScript
+|--------------------------------------------------------------------------
+*/
+
+// setters getters
+
+class user {
+    //constructor
+    constructor(name, age) {
+        this.name = name;
+        this.age = age
+    }
+    // metodos
+    #speak() {
+        return "Hello";
+    }
+    greeting() {
+        return `${this.speak()} ${this.name}`;
+    }
+
+    get #uAge() {
+        return this.age;
+    }
+    set #uAge(n) {
+        this.age = n;
+    }
+}
+
+const bebeloper1 = new user("david", 15);
+console.log(bebeloper1.uAge);
+console.log(bebeloper1.speak);
+console.log(bebeloper1.uAge = 20);
