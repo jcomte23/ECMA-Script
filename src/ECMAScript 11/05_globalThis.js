fbce2b010943b8cb5f720cc4d6ab67c5b841ac47
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-07-18 09:18:29 -0500

    globalThis en ECMAScript 11

---
+++ src/ECMAScript 11/05_globalThis.js
@@ -0,0 +1,10 @@
+/*
+|--------------------------------------------------------------------------
+| ECMAScript
+|--------------------------------------------------------------------------
+*/
+
+console.log(window); // navegador
+console.log(global);  // node
+console.log(selft);// webworker
+console.log(globalThis);
