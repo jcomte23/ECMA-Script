3ee0bca447a497c1745ac7dcdc4f60ab7d202ac6
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-07-18 08:56:49 -0500

    optional chaining ECMAScript11

---
+++ src/ECMAScript 11/01_optionalChaining.js
@@ -0,0 +1,20 @@
+/*
+|--------------------------------------------------------------------------
+| ECMAScript
+|--------------------------------------------------------------------------
+*/
+
+const users = {
+    gndx: {
+        country: "MX"
+    },
+    ana: {
+        country: "CO"
+    },
+    jcomte: {
+        country: "CO",
+        name: "Javier Combita Tellez"
+    }
+}
+
+console.log(users?.bebeloper?.country);
