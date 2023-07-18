832ff48d6be6aa4f66cd20ee0a6942e523e5532a
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-07-18 11:15:01 -0500

    at con ECMAScript 13

---
+++ src/ECMAScript 13/01_at.js
@@ -0,0 +1,13 @@
+/*
+|--------------------------------------------------------------------------
+| ECMAScript
+|--------------------------------------------------------------------------
+*/
+
+const frutas = ['manzana', 'pera', 'naranja', 'plátano'];
+
+// Acceder al primer elemento del array con el índice positivo 0
+console.log(frutas.at(0)); // 'manzana'
+
+// Acceder al último elemento del array con el índice negativo -1
+console.log(frutas.at(-1)); // 'platano'
