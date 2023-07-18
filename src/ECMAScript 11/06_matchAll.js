f6661be4a23c5100a5bdb8c59e3d551331d63ac1
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-07-18 09:26:57 -0500

    matchall en ECMAScript11

---
+++ src/ECMAScript 11/06_matchAll.js
@@ -0,0 +1,13 @@
+/*
+|--------------------------------------------------------------------------
+| ECMAScript
+|--------------------------------------------------------------------------
+*/
+
+const regex = /\b(Apple)+\b/g;
+
+const fruit = "Apple, Banana, Kiwi, Apple, Orange, etc. etc. etc.";
+
+for (const match of fruit.matchAll(regex)) {
+    console.log(match);
+}
